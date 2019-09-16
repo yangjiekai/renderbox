02e8b333bcda733910a6db1645d2241416c51226
kai <jackyangg1514@gmail.com>
2019-09-15 23:23:32 -0400

    combine with drum machine

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
 }
 
+AudioCache player=new AudioCache();
+void _onClicked (String filename){
+  player.play ("$filename.mp3");
+}
+
+List data=[
+
+  'KICK',
+  'TAP',
+  'SNARE',
+  'CLAPZ',
+  'BOOM',
+  'HIHAT',
+  'GUN',
+  'CLAP',
+  'BASS 1',
+  'BASS 2',
+  'ROBOT',
+  'VOX',
+  'PIANO',
+  'WAR',
+  'LISTEN',
+  'SUB',
+
+];
+
 class MyApp extends StatelessWidget {
+  Random random =new Random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Grid(),
@@ -30,19 +60,29 @@ class GridState extends State<Grid> {
   final Set<_Foo> _trackTaped = Set<_Foo>();
 
   _detectTapedItem(PointerEvent event) {
+
     final RenderBox box =  key.currentContext.findRenderObject();
     final result = BoxHitTestResult();
     Offset local = box.globalToLocal(event.position);
+
     if (box.hitTest(result, position: local)) {
       for (final hit in result.path) {
         /// temporary variable so that the [is] allows access of [index]
         final target = hit.target;
-        if (target is _Foo && !_trackTaped.contains(target)) {
+        if ( target is _Foo &&
+            !_trackTaped.contains(target)) {
+
+
           _trackTaped.add(target);
           _selectIndex(target.index);
+
+          _onClicked(data[target.index]);
         }
+
       }
+
     }
+
   }
 
   _selectIndex(int index) {
@@ -54,14 +94,17 @@ class GridState extends State<Grid> {
   @override
   Widget build(BuildContext context) {
     return Listener(
+
       onPointerDown: _detectTapedItem,
       onPointerMove: _detectTapedItem,
       onPointerUp: _clearSelection,
+
       child: GridView.builder(
         key: key,
-        itemCount: 6,
+        itemCount: data.length,
         physics: NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+
           crossAxisCount: 3,
           childAspectRatio: 1.0,
           crossAxisSpacing: 5.0,
@@ -69,6 +112,7 @@ class GridState extends State<Grid> {
         ),
         itemBuilder: (context, index) {
           return Foo(
+
             index: index,
             child: Container(
               color: selectedIndexes.contains(index) ? Colors.red : Colors.blue,
